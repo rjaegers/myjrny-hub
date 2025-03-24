8953311e6ae4a289e87fffcde4c9ee54f495e6b0
Ron <45816308+rjaegers@users.noreply.github.com>
2025-03-24 20:30:33 +0000

    refactor: correctly set-up sai clock for ltdc

--- src/mcu/rcc_setup.rs
+++ src/mcu/rcc_setup.rs
@@ -21,11 +21,12 @@ pub fn stm32f746ng_init() -> Peripherals {
     });
 
     config.rcc.pllsai = Some(rcc::Pll {
-        prediv: rcc::PllPreDiv::DIV25,  // PLL_M Divides the 25MHz HSE to 1MHz
-        mul: rcc::PllMul::MUL384,       // PLL_N Multiplies the 1MHz to 384MHz
-        divp: Some(rcc::PllPDiv::DIV8), // PLL_P Divides the 384MHz to 48MHz for SAI
-        divq: Some(rcc::PllQDiv::DIV2), // PLL_Q Divides the 25MHz HSE to 1MHz
-        divr: Some(rcc::PllRDiv::DIV5), // PLL_R Divides the 384MHz to 76.8MHz for LTDC
+        prediv: rcc::PllPreDiv::DIV25, // PLL_M Divides the 25MHz HSE to 1MHz
+        mul: rcc::PllMul::MUL192,      // PLL_N Multiplies the 1MHz to 192MHz
+        divp: None,                    // PLL_P
+        divq: None,                    // PLL_Q
+        divr: Some(rcc::PllRDiv::DIV4), // PLL_R Divides the 192MHz to 48MHz for LTDC
+                                       // LTDC speed according to AN4861 - Table 11
     });
 
     config.rcc.pll_src = rcc::PllSource::HSE;
